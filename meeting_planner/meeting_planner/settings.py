55bd684a27eed1cdb9abf90ea7538c487866bf00
Juan E Afable <juanjuanzerojuan@gmail.com>
2021-10-22 05:40:55 -0400

    moved secret key to environment variable

--- meeting_planner/meeting_planner/settings.py
+++ meeting_planner/meeting_planner/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v5d$2ss1ygu5t02ijs=nzj7adu8#$3j7t$tu_$)!6n%@71%hh!'
+SECRET_KEY = os.environ['learning_django_secret']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
